e5e86f69e79a44f2a22e670549edbc55f7355303
tj-tieso <tj.tieso@gmail.com>
2022-07-19 14:47:51 -0500

    webpack install

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const webpack = require("webpack");
+const path = require("path");
+
+// create the main configuration object
+module.exports = {
+  entry: "./assets/js/script.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.bundle.js",
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+  ],
+  mode: "development",
+};
